8a54d1087afd103f611d9326b5c4ec03e761e1dc
nagwa52 <nagwatalaat52@gmail.com>
2022-04-25 18:26:20 +0200

    add task scheduling

---
+++ app/Jobs/PruneOldPostsJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
+use App\Models\Post;
+
+class PruneOldPostsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $date = Carbon::now()->subDays(2)->toDateTimeString();
+        Post::where('created_at', '<=', $date)->delete();
+    }
+}
